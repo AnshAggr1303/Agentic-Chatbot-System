323ac6e86cacb0f1c2f85e9fa1ec814345063a2a
yashdagar <yashdagar4557@gmail.com>
2025-08-05 15:05:17 +0530

    change chat font weight

--- app/page.tsx
+++ app/page.tsx
@@ -654,7 +654,7 @@ export default function AudioChatPage() {
                         </div>
                       )}
                       <div
-                        className={`max-w-[90%] flex px-4 py-2 rounded-2xl ${
+                        className={`max-w-[90%] flex px-4 py-2 *:font-bold! rounded-2xl ${
                           message.type === 'user'
                             ? isDarkMode
                               ? 'bg-blue-900 text-gray-200 justify-end'
